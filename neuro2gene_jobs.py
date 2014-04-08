eb7bda6e2ee74e24f0295ca2b24cf8b492ab6656
Vanessa Villamia Sochat <vsochat@sherlock-ln02.stanford.edu>
2014-04-08 14:47:17 -0700

    modified:   neuro2gene_jobs.py

--- neuro2gene_jobs.py
+++ neuro2gene_jobs.py
@@ -24,11 +24,7 @@ for f in features:
   jobfile.writelines("#SBATCH --error=.out/"+ f + "_genes.err\n") 
   jobfile.writelines("#SBATCH --time=2-00:00\n") 
   jobfile.writelines("#SBATCH --mem=12000\n")
-<<<<<<< HEAD
   jobfile.writelines("source /home/vsochat/python-lapack-blas/bin/activate\n")   
-=======
-  jobfile.writelines("source /home/vsochat/python-lapack-blas/activate\n")   
->>>>>>> 2d0c8b2fad1b56cb18f9e8a8ae6612347a98c469
   jobfile.writelines("/home/vsochat/python-lapack-blas/bin/python /home/vsochat/SCRIPT/python/neuro2gene/run_neuro2gene_cluster.py " + f + " " + str(THRESHOLD) + " " + analysisTime + "\n")  
   jobfile.close()
 
